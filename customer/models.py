f8e07359575bcf5d6991b38603e3a21dc77c5ba5
Tri Dang <tridang0402@gmail.com>
2024-11-25 13:19:54 +0700

    commit 2

--- customer/models.py
+++ customer/models.py
@@ -5,7 +5,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     image = models.ImageField(upload_to="menu_images/")
-    price = models.DecimalField(max_digits=9, decimal_places=0)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     category = models.ManyToManyField('Category', related_name='item')
 
     def __str__(self):
@@ -19,7 +19,7 @@ class Category(models.Model):
 
 class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
-    price = models.DecimalField(max_digits=9, decimal_places=0)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     items = models.ManyToManyField(
         'MenuItem', related_name='order', blank=True)
 

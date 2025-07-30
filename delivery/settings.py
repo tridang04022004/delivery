3763229ad932b2a275ef2fbb903b2f3dab61503b
TRucey <113203737+tridang04022004@users.noreply.github.com>
2025-07-30 09:52:08 +0700

    Update settings.py

--- delivery/settings.py
+++ delivery/settings.py
@@ -60,7 +60,12 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+
+
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

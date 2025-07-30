4d197f5201c8bb7fe28d6ac98ba1960304c7bd33
TRucey <113203737+tridang04022004@users.noreply.github.com>
2025-07-30 09:48:03 +0700

    Update settings.py

--- delivery/settings.py
+++ delivery/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for deployment
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # optional for local static folders
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
